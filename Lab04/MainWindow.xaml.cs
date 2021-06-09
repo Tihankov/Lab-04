082d4a343a4c18b3ff2563a9895fb819d6e9e27a
Tihankov <75485902+Tihankov@users.noreply.github.com>
2021-06-09 10:22:09 +0300

    Add files via upload

---
+++ Lab04/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBox1.Text == "" && txtBox2.Text == "")
+            {
+                MessageBox.Show("At least one text box should be with text!", "Error");
+            }
+            else
+            {
+                string text;
+                text = txtBox1.Text;
+                txtBox1.Text = txtBox2.Text;
+                txtBox2.Text = text;
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure?", "Closing...", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+            {
+                Close();
+            }
+        }
+    }
+}
